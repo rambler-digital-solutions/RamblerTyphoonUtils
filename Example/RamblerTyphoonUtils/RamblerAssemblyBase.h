94996f4469486d74ea977aa6555bedff87cc59ff
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-07-18 16:23:05 +0300

    Updated license text

---
+++ Example/RamblerTyphoonUtils/RamblerAssemblyBase.h
@@ -0,0 +1,13 @@
+//
+//  RamblerAssemblyBase.h
+//  RamblerTyphoonUtils
+//
+//  Created by Egor Tolstoy on 18/07/16.
+//  Copyright © 2016 Egor Tolstoy. All rights reserved.
+//
+
+#import <Typhoon/Typhoon.h>
+
+@interface RamblerAssemblyBase : TyphoonAssembly
+
+@end
---
+++ Example/RamblerTyphoonUtils/RamblerAssemblyBase.m
@@ -0,0 +1,13 @@
+//
+//  RamblerAssemblyBase.m
+//  RamblerTyphoonUtils
+//
+//  Created by Egor Tolstoy on 18/07/16.
+//  Copyright © 2016 Egor Tolstoy. All rights reserved.
+//
+
+#import "RamblerAssemblyBase.h"
+
+@implementation RamblerAssemblyBase
+
+@end
